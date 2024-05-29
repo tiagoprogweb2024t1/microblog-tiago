7f83ac68dbb1ea982b3c17128cc342f72f7b774a
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-05-29 16:30:06 -0300

    Update usuario-insere.php

--- admin/usuario-insere.php
+++ admin/usuario-insere.php
@@ -1,5 +1,13 @@
 <?php 
 require_once "../inc/cabecalho-admin.php";
+
+// Importando as funções de manipulação dos usuários
+require_once "../inc/funcoes-usuarios.php";
+
+/* Detectando se o botão inserir foi acionado */
+if(isset($_POST['inserir'])){
+	echo "Ok!";
+}
 ?>
 
 
