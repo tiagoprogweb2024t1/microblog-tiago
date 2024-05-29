7b33ee88a6a472676cbfa45bc7f6199a9f508231
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-05-29 14:38:58 -0300

    Arquivos iniciais

---
+++ inc/rodape-admin.php
@@ -0,0 +1,31 @@
+</div>
+</main>
+
+<footer class="footer mt-auto py-3 bg-dark border-top">
+    <div class="container text-center text-white">
+        Microblog é um site fictício desenvolvido para fins didáticos | Senac Penha &copy; 2024
+    </div>
+</footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+<?php 
+switch($pagina){
+    case 'usuarios.php':
+    case 'noticias.php':
+?>
+<script src="js/confirm.js"></script>
+<?php
+    break;
+
+    case 'noticia-insere.php':
+    case 'noticia-atualiza.php':
+?>
+<script src="js/contador.js"></script>
+<?php
+    break;
+}
+?>
+</body>
+</html>
