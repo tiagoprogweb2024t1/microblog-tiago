1524523ea2453e0003e85918efddf4ecbacd0734
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-06-04 14:15:27 -0300

    Update usuarios.php

--- admin/usuarios.php
+++ admin/usuarios.php
@@ -36,9 +36,9 @@ $listaDeUsuarios = lerUsuarios($conexao);
 
 <?php foreach($listaDeUsuarios as $usuario) { ?>
 					<tr>
-						<td> Nome... </td>
-						<td> E-mail... </td>
-						<td> Tipo... </td>
+						<td> <?=$usuario["nome"]?> </td>
+						<td> <?=$usuario["email"]?> </td>
+						<td> <?=$usuario["tipo"]?> </td>
 						<td class="text-center">
 							<a class="btn btn-warning" 
 							href="usuario-atualiza.php">
