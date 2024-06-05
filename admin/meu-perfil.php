7302539ab6f01320b6ba0b69afcee90648d75643
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-06-05 16:51:52 -0300

    Update meu-perfil.php

--- admin/meu-perfil.php
+++ admin/meu-perfil.php
@@ -1,5 +1,22 @@
 <?php 
 require_once "../inc/cabecalho-admin.php";
+
+/* 1) Carregue as funções de usuários */
+
+/* 2) Pegue o ID do usuário através da SESSÃO */
+
+/* 3) Chame a função lerUmUsuario e guarde o que ela retorna (array de dados) */
+
+/* 4) Programe uma condicional para detectar se o formulário de atualização foi acionado.
+
+	4.1) Capture os dados digitados no formulário (nome, e-mail)
+	4.2) Capture o tipo do usuário através da SESSÃO
+	4.3) Faça a programação condicional necessária para a senha (é o mesmo código usado em usuario-atualiza.php)
+	4.4) Fora da condicional da senha, chame a função atualizarUsuario e passe os dados pra ela
+	4.5) Redirecione para a página index.php (a que está dentro de admin) */
+
+/* 5) DESAFIO: faça com que, ao mudar o nome do usuário, automaticamente apareça o novo nome na index.php */
+
 ?>
 
 
