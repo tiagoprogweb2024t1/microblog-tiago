1ff7ebdc871e8256fd18845c9e72e4c51d9d7b74
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-06-06 17:26:53 -0300

    Update noticias.php

--- admin/noticias.php
+++ admin/noticias.php
@@ -1,7 +1,16 @@
-<?php 
+<?php // noticias.php
 require_once "../inc/cabecalho-admin.php";
-?>
+require_once "../inc/funcoes-noticias.php";
 
+// Pegando o id do usuário logado
+$idUsuario = $_SESSION['id'];
+
+// Pegando o tipo do usuário logado
+$tipoUsuario = $_SESSION['tipo'];
+
+// Chamando a função, passando os parâmetros, e recebendo os dados
+$listaDeNoticias = lerNoticias($conexao, $idUsuario, $tipoUsuario);
+?>
 
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
@@ -30,23 +39,26 @@ require_once "../inc/cabecalho-admin.php";
 
 				<tbody>
 
+<?php foreach($listaDeNoticias as $noticia) { ?>
 					<tr>
-                        <td> Título da notícia... </td>
-                        <td> 21/12/2112 21:12 </td>
-                        <td> Autor da notícia... </td>
+                        <td> <?=$noticia['titulo']?> </td>
+
+                        <td> <?=formataData($noticia['data'])?> </td>
+
+                        <td> <?=$noticia['nome']?> </td>
 						<td class="text-center">
 							<a class="btn btn-warning" 
-							href="noticia-atualiza.php">
+							href="noticia-atualiza.php?id=<?=$noticia['id']?>">
 							<i class="bi bi-pencil"></i> Atualizar
 							</a>
 						
 							<a class="btn btn-danger excluir" 
-							href="noticia-exclui.php">
+							href="noticia-exclui.php?id=<?=$noticia['id']?>">
 							<i class="bi bi-trash"></i> Excluir
 							</a>
 						</td>
 					</tr>
-
+<?php } ?>
 				</tbody>                
 			</table>
 	</div>
