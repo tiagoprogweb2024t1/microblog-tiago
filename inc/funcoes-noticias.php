4693028d23c54e7715d48c102d52ab22ea5c9a8d
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-06-06 13:55:23 -0300

    Create funcoes-noticias.php

---
+++ inc/funcoes-noticias.php
@@ -0,0 +1,12 @@
+<?php
+require "conecta.php";
+
+function inserirNoticia($conexao){}
+
+function lerNoticias($conexao){}
+
+function lerUmaNoticia($conexao){}
+
+function atualizarNoticia($conexao){}
+
+function excluirNoticia($conexao){}
